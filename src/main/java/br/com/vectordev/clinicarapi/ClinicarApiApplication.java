71dc19eea05191535c3d6e1c64c716b690929d44
=washlopes <washlopes@gmail.com>
2020-08-18 16:19:59 -0300

    Projeto inicial do backend do Gerenciador de Clinicas, gerenciamento de atendimentos e agenda.

---
+++ src/main/java/br/com/vectordev/clinicarapi/ClinicarApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.vectordev.clinicarapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinicarApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClinicarApiApplication.class, args);
+	}
+
+}
